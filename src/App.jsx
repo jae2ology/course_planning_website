21791725a892d2b827df6afc51eb1fb3fe7f1e4e
jae2ology <jacksonjae90@gmail.com>
2026-03-18 21:36:07 -0400

    init tailwindcss, daisyui and create the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
+export default function App() {
 
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Peaker</h1>
+                    <p className="py-6">
+                        For those who want ease in scheduling courses.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App
